ce1362fe1ee7ee4af42a00baea2e6c66b79c60f2
webworrk-rm <webworrkteam@gmail.com>
2025-02-05 04:25:23 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoConference from "./components/VideoConference";
+
+function App() {
+  return (
+    <div>
+      <h1>Video Conference App</h1>
+      <VideoConference />
+    </div>
+  );
+}
+
+export default App;
